496099e2fba4cd1995b15c73c45bcc0fcc1e26c3
PradeepGururani <pg@novemberfirst.com>
2020-08-11 17:03:33 +0530

    Modified certain implementation.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace dotnet_api_demo.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Very Hot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
